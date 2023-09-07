99086eec4f6817d0fd6ba66da7a41ce4006c4250
Axel Bresanovich <bresanovichaxe43@gmail.com>
2023-09-08 01:23:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const logger =require("./file_logger/logger");
+
+
+
+//logger.log("hola soy console.log");
+logger.info("hola soy info");
+logger.debug("hola soy debug");
+logger.warn("hola soy warn");
+logger.error("hola soy error");
+
